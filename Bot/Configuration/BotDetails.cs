3b574df49b15ea9c6a3080b693178dc7896b38cb
Pratik Kumar <pratikyoff@gmail.com>
2017-11-30 22:16:43 +0530

    Added basic bot description

---
+++ Bot/Configuration/BotDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Configuration
+{
+    public static class BotDetails
+    {
+        public static string Name { get => "dota-stalker-bot"; }
+        public static string ClientId { get => "356727461385601025"; }
+        public static string ClientSecret { get => "H8Ny3LX0VI24vjo_EL2_u85YCYAYFVBl"; }
+        public static string Username { get => "dota-stalker-bot#3927"; }
+        public static string Token { get => "MzU2NzI3NDYxMzg1NjAxMDI1.DQHGzw.Py7sGQmYgucEq9oCWGnefUQBnRQ"; }
+    }
+}
