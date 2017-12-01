cb0a81132633033862815292dd2cf882ea3747cf
Pratik Kumar <pratikyoff@gmail.com>
2017-12-01 18:40:57 +0530

    Added stop mechanism

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -7,12 +7,36 @@ namespace Bot
     {
         static void Main(string[] args)
         {
+            bool quit = false;
+            Console.CancelKeyPress += (x, y) =>
+            {
+                quit = true;
+            };
             TcpClient tcpClient = new TcpClient();
             TcpClientCache.Cache = tcpClient;
 
             string gatewayUrl = Gateway.GetUrl();
-            Console.WriteLine(gatewayUrl);
-            Console.ReadKey(true);
+            Console.WriteLine("Gateway: " + gatewayUrl);
+
+            tcpClient.ConnectAsync(gatewayUrl.Substring(6), 80).GetAwaiter().GetResult();
+
+            while (!quit)
+            {
+                string read = ReadFromStream(tcpClient);
+            }
+        }
+
+        private static string ReadFromStream(TcpClient tcpClient)
+        {
+            int buffer;
+            string read = string.Empty;
+            NetworkStream stream = tcpClient.GetStream();
+            while (stream.DataAvailable)
+            {
+                buffer = stream.ReadByte();
+                read += (char)buffer;
+            }
+            return read;
         }
     }
 }
