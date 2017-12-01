ee19f308a6ce1c5398fa13ed710205566c95423f
Pratik Kumar <pratikyoff@gmail.com>
2017-12-01 22:00:27 +0530

    Basic logger

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,14 +1,17 @@
-﻿using System;
-using System.IO;
-using System.Net.Http;
-using System.Net;
-using System.Text;
-using System.Net.Sockets;
+﻿using Bot.Contracts;
+using Bot.Implementations;
+using DSharpPlus;
+using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
 
 namespace Bot
 {
     class Program
     {
+        static DiscordClient discord;
+        static ILogger logger;
+
         static void Main(string[] args)
         {
             bool quit = false;
@@ -17,12 +20,42 @@ namespace Bot
                 quit = true;
             };
 
-            string gatewayUrl = Gateway.GetUrl();
-            Console.WriteLine("Gateway: " + gatewayUrl);
+            logger = new ConsoleLogger();
 
-            while (!quit)
+            AsyncMain();
+
+            while (!quit) { }
+            discord.DisconnectAsync().GetAwaiter().GetResult();
+            discord.Dispose();
+        }
+
+        private static async Task AsyncMain()
+        {
+            discord = new DiscordClient(new DiscordConfiguration()
             {
-            }
+                Token = BotDetails.Token,
+                TokenType = TokenType.Bot
+            });
+
+            await discord.ConnectAsync();
+            logger.Log("Discord Connected");
+
+            discord.MessageCreated += async x =>
+            {
+                if (x.Message.Content.IndexOf(BotDetails.CommandPrefix) == 0)
+                {
+                    logger.Log(JsonConvert.SerializeObject(x.Message));
+                    string command = GetFirstWord(x.Message.Content);
+
+                }
+            };
+        }
+
+        private static string GetFirstWord(string content)
+        {
+            string sentence = content.Substring(BotDetails.CommandPrefix.Length);
+            string[] words = sentence.Split(' ');
+            return words[0];
         }
     }
 }
