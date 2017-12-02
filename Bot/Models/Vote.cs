e9f45d38f7715241ec27d801fcc0516f2737c36e
Pratik Kumar <pratikyoff@gmail.com>
2017-12-03 02:49:03 +0530

    Vote functionality completed.

--- Bot/Models/Vote.cs
+++ Bot/Models/Vote.cs
@@ -9,5 +9,6 @@ namespace Bot.Models
         public Dictionary<int, Option> Options { get; set; } = new Dictionary<int, Option>();
         public bool InProgress { get; set; } = false;
         public int TimeoutSeconds { get => 60; }
+        public List<string> UserVotedStore { get; set; } = new List<string>();
     }
 }
