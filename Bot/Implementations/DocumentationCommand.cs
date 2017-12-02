745b5d79608e34cea3313b998707d1a0030d45e8
Pratik Kumar <pratikyoff@gmail.com>
2017-12-03 03:25:21 +0530

    Added documentation functionality.

---
+++ Bot/Implementations/DocumentationCommand.cs
@@ -0,0 +1,22 @@
+﻿using Bot.Contracts;
+using System;
+using DSharpPlus.Entities;
+using Bot.Configuration;
+
+namespace Bot.Implementations
+{
+    public class DocumentationCommand : ICommand
+    {
+        string doc = $"Here are a list of commands presently supported.\n" +
+            $"1.Ping - `!{CommandConfiguration.PingCommandString}` command for checking the latency of the bot.\n" +
+            $"2.Vote - `!{CommandConfiguration.VoteCommandString}` command for\n" +
+            $"\t1.Initiating a vote - `!{CommandConfiguration.VoteCommandString} <Vote subject> <option1>|<option2>|<option3>`\n" +
+            $"\t2.Voting - `!{CommandConfiguration.VoteCommandString} <optionNumber>` eg `!{CommandConfiguration.VoteCommandString} 2`\n" +
+            $"\t3.Status - `!{CommandConfiguration.VoteCommandString} status`\n";
+
+        public string Process(DiscordMessage message)
+        {
+            return doc;
+        }
+    }
+}
