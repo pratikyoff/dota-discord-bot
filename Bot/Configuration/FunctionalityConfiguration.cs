097efc640c052e9a24b0a6e58707f982463f75d5
Pratik Kumar <pratikyoff@gmail.com>
2017-12-03 13:22:19 +0530

    Added support for functionalities

---
+++ Bot/Configuration/FunctionalityConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Bot.Contracts;
+using Bot.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Configuration
+{
+    public static class FunctionalityConfiguration
+    {
+        private static Functionality _gametimeTracker = new GametimeTracker();
+        public static List<Functionality> Functionalities { get; set; } = new List<Functionality>()
+        {
+            _gametimeTracker,
+        };
+    }
+}
---
+++ Bot/Contracts/Functionality.cs
@@ -0,0 +1,24 @@
+﻿using DSharpPlus;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bot.Contracts
+{
+    public abstract class Functionality
+    {
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+        public abstract void Run(DiscordClient discord);
+        public void Start(DiscordClient discord)
+        {
+            CancellationToken _cancellationToken = _cancellationTokenSource.Token;
+            Task.Factory.StartNew(() =>
+            {
+                Run(discord);
+            }, _cancellationToken);
+        }
+        public void Stop()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
