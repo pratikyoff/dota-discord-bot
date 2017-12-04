2a78f80effbdabed9cd79a5f09591a12f95db622
Pratik Kumar <pratikyoff@gmail.com>
2017-12-04 11:46:40 +0530

    Added list of players

---
+++ Bot/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public string DiscordId { get; set; }
+        public string SteamId { get; set; }
+    }
+}
