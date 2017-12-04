64d44517260917b063581c822c68eadc09340715
Pratik Kumar <pratikyoff@gmail.com>
2017-12-04 10:21:18 +0530

    Basic starter for getting all members

---
+++ Bot/Configuration/GuildConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bot.Configuration
+{
+    public static class GuildConfiguration
+    {
+        public static ulong Id { get => 234246313741385728; }
+    }
+}
--- Bot/Implementations/GametimeTracker.cs
+++ Bot/Implementations/GametimeTracker.cs
@@ -4,14 +4,30 @@ using System.Collections.Generic;
 using System.Text;
 using DSharpPlus.Entities;
 using DSharpPlus;
+using Bot.Configuration;
+using System.Linq;
 
 namespace Bot.Implementations
 {
     public class GametimeTracker : Functionality
     {
+        private Dictionary<DiscordMember, object> _gamePlaying = new Dictionary<DiscordMember, object>();
+
         public override void Run(DiscordClient discord)
         {
-            throw new NotImplementedException();
+            var members = GetAllNonBotMembers(discord);
+            foreach (var member in members)
+            {
+                var currentInfo = discord.GetUserAsync(member.Id).GetAwaiter().GetResult();
+                _gamePlaying[member] = member.Presence;
+            }
+        }
+
+        private List<DiscordMember> GetAllNonBotMembers(DiscordClient discord)
+        {
+            DiscordGuild guild = discord.GetGuildAsync(GuildConfiguration.Id).GetAwaiter().GetResult();
+            var members = guild.GetAllMembersAsync().GetAwaiter().GetResult().Where(x => !x.IsBot).ToList();
+            return members;
         }
     }
 }
