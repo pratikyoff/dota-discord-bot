74ad5d737502c94bf943bb986ed113ced8e4bafd
Pratik Kumar <pkumar@tavisca.com>
2017-12-05 13:55:03 +0530

    Implemented cache limiting in key value pair. Fixes #3

--- Bot/Cache/KeyValueCache.cs
+++ Bot/Cache/KeyValueCache.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Bot
@@ -7,6 +8,8 @@ namespace Bot
     public static class KeyValueCache
     {
         private static Dictionary<string, string> _cache = new Dictionary<string, string>();
+        private static List<string> _keys = new List<string>();
+        private static int _limit = 100;
 
         public static string Get(string key)
         {
@@ -19,7 +22,13 @@ namespace Bot
         {
             if (Get(key) != null)
             {
+                if (_keys.Count >= _limit)
+                {
+                    _cache.Remove(_keys.Last());
+                    _keys.RemoveAt(_keys.Count);
+                }
                 _cache[key] = value;
+                _keys.Add(key);
                 return true;
             }
             else
