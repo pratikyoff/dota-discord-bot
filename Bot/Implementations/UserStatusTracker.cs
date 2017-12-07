ec00bb88ba193187f15fa762f42f0540bc382e0c
Pratik Kumar <pkumar@tavisca.com>
2017-12-07 12:06:23 +0530

    Added a new functionality and disabled the bugged functionalities.

---
+++ Bot/Implementations/UserStatusTracker.cs
@@ -0,0 +1,26 @@
+﻿using Bot.Contracts;
+using System;
+using DSharpPlus;
+using System.Collections.Generic;
+using DSharpPlus.Entities;
+using Bot.Configuration;
+using System.Linq;
+
+namespace Bot.Implementations
+{
+    public class UserStatusTracker : Functionality
+    {
+        public override void Run(DiscordClient discord)
+        {
+            var members = GetAllNonBotMembers(discord);
+            members.ForEach(x => Console.WriteLine(x.Presence));
+        }
+
+        public List<DiscordMember> GetAllNonBotMembers(DiscordClient discord)
+        {
+            DiscordGuild guild = discord.GetGuildAsync(GuildConfiguration.Id).GetAwaiter().GetResult();
+            var members = guild.GetAllMembersAsync().GetAwaiter().GetResult().Where(x => !x.IsBot).ToList();
+            return members;
+        }
+    }
+}
