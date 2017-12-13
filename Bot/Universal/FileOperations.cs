d4d47ce938874828ade5a77c44f474e67ba5d547
Pratik Kumar <pkumar@tavisca.com>
2017-12-13 14:03:12 +0530

    New helper class for file operations

---
+++ Bot/Universal/FileOperations.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace Bot.Universal
+{
+    public static class FileOperations
+    {
+        public static int GetTotalNoOfLinesInFile(string fileName)
+        {
+            int lineCount = File.ReadLines(fileName).Count();
+            return lineCount;
+        }
+
+        public static string ReadLineNo(string fileName, int lineIndex)
+        {
+            if (lineIndex < 0 || lineIndex >= GetTotalNoOfLinesInFile(fileName))
+                return null;
+            return File.ReadLines(fileName).ElementAt(lineIndex);
+        }
+
+        public static bool AppendLine(string fileName, string line)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName, true))
+            {
+                sw.WriteLine(line);
+            }
+            return true;
+        }
+
+        public static bool DeleteLine(string fileName, int lineIndex)
+        {
+            if (lineIndex < 0 || lineIndex >= GetTotalNoOfLinesInFile(fileName))
+                return false;
+            string line = null;
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                using (StreamWriter sw = new StreamWriter("temp.log"))
+                {
+                    for (int i = 0; (line = sr.ReadLine()) != null; i++)
+                    {
+                        if (i == lineIndex) continue;
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+            File.Delete(fileName);
+            File.Move("temp.log", fileName);
+            return true;
+        }
+    }
+}
---
+++ BotFixture/FileOperationsFixture.cs
@@ -0,0 +1,47 @@
+﻿using Bot.Universal;
+using System.IO;
+using Xunit;
+
+namespace BotFixture
+{
+    public class FileOperationsFixture
+    {
+        [Fact]
+        public void TotalLineFixture()
+        {
+            string filename = "newfile.txt";
+            File.Delete(filename);
+            FileOperations.AppendLine(filename, filename);
+            FileOperations.AppendLine(filename, filename);
+            FileOperations.AppendLine(filename, filename);
+            Assert.Equal(3, FileOperations.GetTotalNoOfLinesInFile(filename));
+            File.Delete(filename);
+        }
+
+        [Fact]
+        public void DeleteLineFixture()
+        {
+            string filename = "newfile.txt";
+            File.Delete(filename);
+            FileOperations.AppendLine(filename, "1");
+            FileOperations.AppendLine(filename, "2");
+            FileOperations.AppendLine(filename, "3");
+            FileOperations.DeleteLine(filename, 1);
+            Assert.Equal("3", FileOperations.ReadLineNo(filename, 1));
+            File.Delete(filename);
+        }
+
+        [Fact]
+        public void ReadLineFixture()
+        {
+            string filename = "newfile.txt";
+            File.Delete(filename);
+            FileOperations.AppendLine(filename, "1");
+            FileOperations.AppendLine(filename, "2");
+            FileOperations.AppendLine(filename, "3");
+            FileOperations.AppendLine(filename, "4");
+            Assert.Equal("4", FileOperations.ReadLineNo(filename, 3));
+            File.Delete(filename);
+        }
+    }
+}
