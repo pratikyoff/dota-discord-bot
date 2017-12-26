fbb33f950a18008a957c9b85af5c553ddccdad74
Pratik Kumar <pkumar@tavisca.com>
2017-12-26 16:43:29 +0530

    Spam control enabled with default setting of 2 seconds.

---
+++ Bot/Implementations/Functionality/UnDuplicator.cs
@@ -0,0 +1,32 @@
+﻿using Bot.Contracts;
+using System.Collections.Generic;
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+namespace Bot.Implementations
+{
+    public class UnDuplicator : Functionality
+    {
+        private Dictionary<ulong, DiscordMessage> _lastMessage = new Dictionary<ulong, DiscordMessage>();
+        public override void Run(DiscordClient discord)
+        {
+            Program.Discord.MessageCreated += async x =>
+            {
+                if (_lastMessage.ContainsKey(x.Author.Id))
+                {
+                    var timeDiff = x.Message.CreationTimestamp - _lastMessage[x.Author.Id].CreationTimestamp;
+                    if (timeDiff.TotalSeconds < 2 && _lastMessage[x.Author.Id].Content.Equals(x.Message.Content))
+                    {
+                        try
+                        {
+                            await x.Message.DeleteAsync();
+                            Program.logger.Log($"Spam controlled from {x.Author.Username} <{x.Author.Id}>");
+                        }
+                        catch { }
+                    }
+                }
+                _lastMessage[x.Author.Id] = x.Message;
+            };
+        }
+    }
+}
