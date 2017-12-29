a4821f6bd45b003891bf3ca296a03fbd1b130338
Pratik Kumar <pkumar@tavisca.com>
2017-12-29 09:56:58 +0530

    Added token to an ignored file

--- Bot/Configuration/BotDetails.cs
+++ Bot/Configuration/BotDetails.cs
@@ -1,4 +1,6 @@
-﻿namespace Bot
+﻿using Bot.Universal;
+
+namespace Bot
 {
     public static class BotDetails
     {
@@ -6,7 +8,7 @@
         public static string ClientId { get => "356727461385601025"; }
         public static string ClientSecret { get => "H8Ny3LX0VI24vjo_EL2_u85YCYAYFVBl"; }
         public static string Username { get => "dota-stalker-bot#3927"; }
-        public static string Token { get => "MzU2NzI3NDYxMzg1NjAxMDI1.DQHGzw.Py7sGQmYgucEq9oCWGnefUQBnRQ"; }
+        public static string Token { get => FileOperations.ReadLineNo("token.txt", 0); }
         public static string ApiUrl { get => "https://discordapp.com/api/"; }
         public static string CommandPrefix { get => "!"; }
         public static ulong BotFeedChannel { get => 387244251936849930; }
