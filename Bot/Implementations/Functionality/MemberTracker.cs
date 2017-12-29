790162c435417a0a331703d61ac4dbca91b5c37c
Pratik Kumar <pkumar@tavisca.com>
2017-12-29 16:19:09 +0530

    Basic tracker for members of the guild

---
+++ Bot/Implementations/Functionality/MemberTracker.cs
@@ -0,0 +1,29 @@
+﻿using Bot.Contracts;
+using System.Collections.Generic;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using Bot.Configuration;
+
+namespace Bot.Implementations
+{
+    public class MemberTracker : Functionality
+    {
+        public static Dictionary<ulong, DiscordMember> Members = new Dictionary<ulong, DiscordMember>();
+
+        public MemberTracker()
+        {
+            DiscordGuild guild = Program.Discord.GetGuildAsync(GuildConfiguration.Id).GetAwaiter().GetResult();
+            var members = guild.Members;
+            foreach (var member in members)
+            {
+                Members[member.Id] = member;
+            }
+        }
+
+        public override void Run(DiscordClient discord)
+        {
+            Program.Discord.GuildMemberAdded += async x => Members[x.Member.Id] = x.Member;
+            Program.Discord.GuildMemberRemoved += async x => Members.Remove(x.Member.Id);
+        }
+    }
+}
