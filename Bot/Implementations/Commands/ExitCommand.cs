9b0432394245aa7f66564581597016b2f58fe107
Pratik Kumar <pkumar@tavisca.com>
2018-01-11 13:41:34 +0530

    Added authorization for exit command

--- Bot/Implementations/Commands/ExitCommand.cs
+++ Bot/Implementations/Commands/ExitCommand.cs
@@ -12,8 +12,10 @@ namespace Bot.Implementations
     {
         public Task<string> Process(DiscordMessage message)
         {
+            if (!message.Author.Id.ToString().Equals("162522319737061376"))
+                return Task.FromResult("Not authorized");
             Program.CancellationTokenSource.CancelAfter(1 * 1000);
-            return Task.FromResult("Exiting");
+            return Task.FromResult("Exiting.");
         }
     }
 }
