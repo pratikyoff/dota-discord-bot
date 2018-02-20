21fa3daefe787959f8f9eb25a07ea23c6adee578
Pratik Kumar <pratikyoff@gmail.com>
2018-02-20 17:54:22 +0530

    Started new functionality OfflineGameTracker

---
+++ Bot/Implementations/Functionality/OfflineTracker.cs
@@ -0,0 +1,18 @@
+﻿using Bot.Contracts;
+using DSharpPlus;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Bot.Implementations
+{
+    public class OfflineTracker : Functionality
+    {
+        public override async void Run(DiscordClient discord)
+        {
+            var channel = await discord.GetChannelAsync(BotDetails.BotDumpChannel);
+            var lastMessage = (await channel.GetMessagesAsync(1)).FirstOrDefault();
+            var matchId = Regex.Match(lastMessage.Content, $"[0-9]+$").Value;
+
+        }
+    }
+}
