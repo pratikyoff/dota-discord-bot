9451977ff489241701c595f92c0d66f0c56a9a35
Pratik Kumar <pkumar@tavisca.com>
2018-02-21 16:35:29 +0530

    progress on OfflineTracker

--- Bot/Implementations/Functionality/OfflineTracker.cs
+++ Bot/Implementations/Functionality/OfflineTracker.cs
@@ -1,18 +1,38 @@
-﻿using Bot.Contracts;
+﻿using Bot.Configuration;
+using Bot.Contracts;
+using Bot.Universal;
 using DSharpPlus;
+using System;
 using System.Linq;
+using System.Net.Http;
 using System.Text.RegularExpressions;
 
 namespace Bot.Implementations
 {
     public class OfflineTracker : Functionality
     {
+        private HttpClient _httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri(OpenDotaConfiguration.OpenDotaAPIAddress)
+        };
         public override async void Run(DiscordClient discord)
         {
             var channel = await discord.GetChannelAsync(BotDetails.BotDumpChannel);
             var lastMessage = (await channel.GetMessagesAsync(1)).FirstOrDefault();
             var matchId = Regex.Match(lastMessage.Content, $"[0-9]+$").Value;
+            string matchDetailsString = KeyValueCache.Get(matchId);
+            if (matchDetailsString == null)
+            {
+                matchDetailsString = await NetComm.GetResponseOfURL($"matches/{matchId}", _httpClient);
+                KeyValueCache.Put(matchId, matchDetailsString);
+            }
+            dynamic matchDetails = JsonToFrom.FromJson<dynamic>(matchDetailsString);
+            int matchEndTime = (int)matchDetails.start_time + (int)matchDetails.duration;
+            foreach (var player in PlayerConfiguration.Players)
+            {
+                var recentMatches = JsonToFrom.FromJson<dynamic>(await NetComm.GetResponseOfURL($"matches/{matchId}", _httpClient));
 
+            }
         }
     }
 }
---
+++ BotFixture/OfflineTrackerFixture.cs
@@ -0,0 +1,25 @@
+﻿using Bot.Implementations;
+using DSharpPlus;
+using DSharpPlus.Entities;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BotFixture
+{
+    public class OfflineTrackerFixture
+    {
+        [Fact]
+        public void RunFixture()
+        {
+            Mock<DiscordMessage> mockMessage = new Mock<DiscordMessage>();
+            mockMessage.SetupGet(x => x.Content).Returns(MatchDetailsExample.Example);
+            Mock<DiscordChannel> mockChannel = new Mock<DiscordChannel>();
+            mockChannel.Setup(x => x.GetMessagesAsync(It.IsAny<int>(), null, null, null)).ReturnsAsync(new List<DiscordMessage>() { mockMessage.Object });
+            Mock<DiscordClient> mockClient = new Mock<DiscordClient>();
+            mockClient.Setup(x => x.GetChannelAsync(It.IsAny<ulong>())).ReturnsAsync(mockChannel.Object);
+            OfflineTracker offlineTracker = new OfflineTracker();
+            offlineTracker.Start(mockClient.Object);
+        }
+    }
+}
