743ce6b7c823fe5d6d598db79808302f018a9733
Pratik Kumar <pratikyoff@gmail.com>
2018-02-21 18:37:34 +0530

    Fetched match details of players.

--- Bot/Implementations/Functionality/OfflineTracker.cs
+++ Bot/Implementations/Functionality/OfflineTracker.cs
@@ -3,6 +3,7 @@ using Bot.Contracts;
 using Bot.Universal;
 using DSharpPlus;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
@@ -15,6 +16,8 @@ namespace Bot.Implementations
         {
             BaseAddress = new Uri(OpenDotaConfiguration.OpenDotaAPIAddress)
         };
+        private Dictionary<string, dynamic> _matchMap = new Dictionary<string, dynamic>();
+
         public override async void Run(DiscordClient discord)
         {
             var channel = await discord.GetChannelAsync(BotDetails.BotDumpChannel);
@@ -28,11 +31,22 @@ namespace Bot.Implementations
             }
             dynamic matchDetails = JsonToFrom.FromJson<dynamic>(matchDetailsString);
             int matchEndTime = (int)matchDetails.start_time + (int)matchDetails.duration;
+            double diffInTime = GetDiffInDays(matchEndTime);
             foreach (var player in PlayerConfiguration.Players)
             {
-                var recentMatches = JsonToFrom.FromJson<dynamic>(await NetComm.GetResponseOfURL($"matches/{matchId}", _httpClient));
-
+                var recentMatches = JsonToFrom.FromJson<dynamic>(await NetComm.GetResponseOfURL($"players/{player.SteamId}/matches?date={diffInTime}", _httpClient));
+                foreach (var match in recentMatches)
+                {
+                }
             }
+        }
+
+        private static double GetDiffInDays(int epochTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            epoch = epoch.AddSeconds(epochTime);
+            var diffInTime = (DateTime.UtcNow - epoch).TotalDays;
+            return diffInTime;
         }
     }
 }
