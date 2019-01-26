8e7b3488492fc89f86350cffce2f9dd1c753ef1d
Pratik Kumar <pratikyoff@gmail.com>
2019-01-26 16:40:58 +0530

    Changed player configuration

--- Bot/Configuration/PlayerConfiguration.cs
+++ Bot/Configuration/PlayerConfiguration.cs
@@ -14,7 +14,8 @@ namespace Bot.Configuration
             new Player(){Name="pkyo",DiscordId="162522319737061376",SteamId="190500077"},
             new Player(){Name="vartak",DiscordId="260725572173430784",SteamId="222073001"},
             new Player(){Name="mittu boy",DiscordId="271245661574397952",SteamId="374869768"},
-            new Player(){Name="cocky",DiscordId="252827119313354754",SteamId="183398205"}
+            new Player(){Name="cocky",DiscordId="252827119313354754",SteamId="183398205"},
+            new Player(){Name="tushar",DiscordId="460080097299267584",SteamId="443689621"}
         };
     }
 }
